027ee2d6b703cfcbaee6dde62e5ba3d063aa7bc6
Emmanuel Mukeni <emmanuelmukeni2016@gmail.com>
2023-03-16 14:49:07 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - its the main function that serve as the starting point
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
