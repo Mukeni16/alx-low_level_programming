c70a854a2a91adc2f3f8a4eaf57809b39035c024
Emmanuel Mukeni <emmanuelmukeni2016@gmail.com>
2023-03-28 04:22:37 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_puts - function that prints a string
+ *@str: pointer to the string
+ *Return: string and new line
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
